b70131473f76cae2a7e48a922c831ebb6713032f
Leikimiya <Leikimiya@mail.ru>
2023-06-03 23:34:01 +0300

    New commit 23:33

--- js/script.js
+++ js/script.js
@@ -1,11 +1,36 @@
 function Logout() {
-    document.getElementById("logout").classList.toggle("show");
+    document.getElementById('logout').classList.toggle('show');
 };
 
 
-window.onclick = function (event) {
 
-    if (document.getElementById("logout").classList.contains("show")) {
-        alert ('dsdsd')
+// window.onclick = function (event) {
+//     if (document.getElementById('logout').classList.contains('show')) {
+//         document.getElementById('logout').classList.remove('show')
+//     }
+// }
+
+
+
+// document.onclick = function (event){
+//     var block = document.getElementById('logout');
+//     if (event.target.className !== 'logout' &&  block.style.display !== 'none' ){
+//         block.style.display = 'none';
+//     }
+//   };
+
+// let  logouthidden = document.getElementById("logout")
+// let ckicked = document.body
+// document.addEventListener('click', (e) => {
+//     if(e.target === ckicked) {
+//         logouthidden.classList.remove("show");
+//     }
+// });
+
+
+window.addEventListener('click', e => {
+    const target = e.target
+    if (!target.closest('.UserButton') && !target.closest('.logout')) {
+      document.getElementById('logout').classList.remove('show')
     }
-};    
+  })
